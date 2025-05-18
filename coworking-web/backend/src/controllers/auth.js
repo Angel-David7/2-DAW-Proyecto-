5c4259ec09c36b86c02eca02d6de1626d3ff8153
Angel-David7 <atsepser7@gmail.com>
2025-05-18 23:56:24 +0100

    Update auth.js

--- coworking-web/backend/src/controllers/auth.js
+++ coworking-web/backend/src/controllers/auth.js
@@ -6,7 +6,8 @@ exports.register = async (req, res, next) => {
   try {
     const { name, email, password } = req.body;
     const hash = await bcrypt.hash(password, 10);
-    const [user] = await db('users').insert({ name, email, password: hash }, ['id','role']);
+    const [user] = await db('users')
+      .insert({ name, email, password: hash }, ['id','role']);
     const token = jwt.sign({ id: user.id, role: user.role }, process.env.JWT_SECRET);
     res.json({ token });
   } catch (err) { next(err); }
